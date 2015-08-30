0fa7a2dde75434be15c03902e57f3bef5624d7fe
Kevin Fich <KFich@Kevins-MacBook-Pro-2.local>
2015-08-30 18:16:35 -0400

    Initial Commit

---
+++ StoryOfMyLife0.1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StoryOfMyLife0.1
+//
+//  Created by Kevin Fich on 8/30/15.
+//  Copyright (c) 2015 Kevin Fich. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
